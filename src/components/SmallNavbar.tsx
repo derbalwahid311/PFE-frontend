77a76b87baf2a7a62ec50fb37d4de6de47257411
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-04-27 20:11:52 +0100

    fixing errors

--- src/components/SmallNavbar.tsx
+++ src/components/SmallNavbar.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react"
+
 export default function SmallNavbar(){
+    const [open, setOpen] = useState(false)
+
+    function handleMenu(){
+        if(open === false) return setOpen(true)
+        else return setOpen(false)
+    }
     return(
         <div className="h-16 bg-white px-6 py-1 flex justify-between">
             <a href="#"><img className="h-full" src="https://upload.wikimedia.org/wikipedia/commons/2/25/LOGO_UNIV_ORAN_1_Anglais.png" alt="" /></a>
             <img className="h-3 my-auto" src="https://res.cloudinary.com/dbiudcrba/image/upload/v1682616730/burger_gjirio.svg" alt="" />
+            <button onClick={handleMenu}><img className="h-fit my-auto" src="https://res.cloudinary.com/dbiudcrba/image/upload/v1682616730/burger_gjirio.svg" alt="" /></button>
+
             {/*<ul className="flex space-x-5 my-auto">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Test</a></li>
