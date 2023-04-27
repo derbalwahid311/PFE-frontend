5cf953d01214102bfd2dab5326500c18d6e34b39
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-04-27 20:16:40 +0100

    fixing errors

--- src/components/SmallNavbar.tsx
+++ src/components/SmallNavbar.tsx
@@ -10,7 +10,7 @@ export default function SmallNavbar(){
     return(
         <div className="h-16 bg-white px-6 py-1 flex justify-between">
             <a href="#"><img className="h-full" src="https://upload.wikimedia.org/wikipedia/commons/2/25/LOGO_UNIV_ORAN_1_Anglais.png" alt="" /></a>
-            <img className="h-3 my-auto" src="https://res.cloudinary.com/dbiudcrba/image/upload/v1682616730/burger_gjirio.svg" alt="" />
+            {open === false ? <h1 className="my-auto text-red-500">Closed</h1> : <h1 className="my-auto text-green-500">Open</h1>}
             <button onClick={handleMenu}><img className="h-fit my-auto" src="https://res.cloudinary.com/dbiudcrba/image/upload/v1682616730/burger_gjirio.svg" alt="" /></button>
 
             {/*<ul className="flex space-x-5 my-auto">
