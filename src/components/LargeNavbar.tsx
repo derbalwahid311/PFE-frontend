083c6e83cd54ab0ca61d811cc9b776bfec67bf16
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-04-30 21:32:30 +0100

    large navbar prototyping

--- src/components/LargeNavbar.tsx
+++ src/components/LargeNavbar.tsx
@@ -1,13 +1,20 @@
 export default function LargeNavbar(){
     return(
         <>
-        <div className="h-24 bg-white px-10 py-1 flex justify-between">
-            <a href="#"><img className="h-full" src="https://upload.wikimedia.org/wikipedia/commons/2/25/LOGO_UNIV_ORAN_1_Anglais.png" alt="" /></a>
-            <ul className="flex space-x-5 my-auto">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Test</a></li>
-                <li><a href="#">Wee</a></li>
-            </ul>
+        <div className="h-24 font-[Inter] text-white bg-[#080E06] flex justify-between">
+            <a className="my-auto" href="#">
+                <div className="flex my-auto">
+                    <img className="h-16 pl-8 px-3 my-2 border-r-[1px]" src="https://res.cloudinary.com/dbiudcrba/image/upload/v1682879624/Uni_Logo_copy_n0fnmu.png" alt="logo" />
+                    <div className="flex-col my-auto px-3">
+                            <h1 className="text-xs">DEPARTMENT OF</h1>
+                            <h1 className="text-2xl">COMPUTER SCIENCE</h1>
+                    </div>
+                </div>
+            </a>
+            <div className="flex cursor-pointer bg-[#2c302b] hover:bg-[#474b46] px-6 space-x-5">
+                <img className="h-3 my-auto" src="https://res.cloudinary.com/dbiudcrba/image/upload/v1682882755/Vector_l5lf1a.svg" alt="" />
+                <h1 className="my-auto">Menu</h1>
+            </div>
         </div>
         </>
     )
