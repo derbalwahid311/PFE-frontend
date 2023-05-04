8cff664639864f8b30d451ea7656331cb9d00b9d
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-05-04 09:42:11 +0100

    Modification to on mount animation
    On hover animation for menu items

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -18,12 +18,13 @@ export default function Menu({state, handlestate}:any){
                 </div>
     
 
-                <motion.div initial={{y:-100}} animate={{y:0}} className=" text-white px-8 flex-col space-y-6 text-4xl w-fit h-96 font-[Merriweather]">
-                    <motion.h1 initial={{y:-30, opacity:0}} animate={{y:0, opacity:1}} transition={{delay:0.15, duration:0.2, opacity:{duration:0.7, delay:0.15}}} className="cursor-pointer">S'identifier</motion.h1>
-                    <motion.h1 initial={{y:-30, opacity:0}} animate={{y:0, opacity:1}} transition={{delay:0.16, duration:0.2, opacity:{duration:0.7, delay:0.15}}} className="cursor-pointer">Demande</motion.h1>
-                    <motion.h1 initial={{y:-30, opacity:0}} animate={{y:0, opacity:1}} transition={{delay:0.17, duration:0.2, opacity:{duration:0.7, delay:0.15}}} className="cursor-pointer">Inscription</motion.h1>
-                    <motion.h1 initial={{y:-30, opacity:0}} animate={{y:0, opacity:1}} transition={{delay:0.18, duration:0.2, opacity:{duration:0.7, delay:0.15}}} className="cursor-pointer">Recherche</motion.h1>
-                    <motion.h1 initial={{y:-30, opacity:0}} animate={{y:0, opacity:1}} transition={{delay:0.19, duration:0.2, opacity:{duration:0.7, delay:0.15}}} className="cursor-pointer">Post-Graduation</motion.h1>
+                <motion.div initial={{y:-50}} animate={{y:0}} transition={{duration:0.4}} className=" text-white px-8 flex-col space-y-6 text-4xl w-fit h-96 font-[Merriweather]">
+                    <motion.h1 initial={{y:-30, opacity:0}} animate={{y:0, opacity:1}} transition={{delay:0.15, duration:0.2, opacity:{duration:0.7, delay:0.15}}} className="cursor-pointer group w-fit">S'identifier<span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-[0.5px] bg-gray-200"></span></motion.h1>
+                    <motion.h1 initial={{y:-30, opacity:0}} animate={{y:0, opacity:1}} transition={{delay:0.16, duration:0.2, opacity:{duration:0.7, delay:0.15}}} className="cursor-pointer group w-fit">Demande<span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-[0.5px] bg-gray-200"></span></motion.h1>
+                    <motion.h1 initial={{y:-30, opacity:0}} animate={{y:0, opacity:1}} transition={{delay:0.17, duration:0.2, opacity:{duration:0.7, delay:0.15}}} className="cursor-pointer group w-fit">Inscription<span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-[0.5px] bg-gray-200"></span></motion.h1>
+                    <motion.h1 initial={{y:-30, opacity:0}} animate={{y:0, opacity:1}} transition={{delay:0.18, duration:0.2, opacity:{duration:0.7, delay:0.15}}} className="cursor-pointer group w-fit">Recherche<span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-[0.5px] bg-gray-200"></span></motion.h1>
+                    <motion.h1 initial={{y:-30, opacity:0}} animate={{y:0, opacity:1}} transition={{delay:0.19, duration:0.2, opacity:{duration:0.7, delay:0.15}}} className="cursor-pointer group w-fit">Post-Graduation<span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-[0.5px] bg-gray-200"></span></motion.h1>
+
 
                 </motion.div>
 
